7038cdc1f1258ecf37e767228f2d6155ee4524bc
jyoti pal <paljyoti0129@gmail.com>
2023-05-29 16:37:38 +0530

    completed auth controller

---
+++ backend/controller/authController.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../model/user");
+
+// register
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "please add all fields" });
+    }
+
+    //check if user exist
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      res.status(400).json({ message: "User already exixt" });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create user
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "invalid user" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  //check for user email
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      // password: user.password,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message: "Unauthorized access" });
+  }
+};
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SEC, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = {
+  register,
+  login,
+};
