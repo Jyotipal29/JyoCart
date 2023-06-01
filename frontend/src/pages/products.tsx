934de084a50ac538ba3edd82bd42169ee039b9ff
jyoti pal <paljyoti0129@gmail.com>
2023-06-01 11:12:04 +0530

    added heading in products page

--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -24,19 +24,26 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="flex  gap-4  container mx-auto px-2 mt-20 relative  ">
-      {loading ? (
-        <Loader loading={loading} />
-      ) : (
-        <div className="flex-1">
-          <div className="grid lg:grid-cols-4  md:grid-cols-3 grid-cols-2">
-            {products.map((item) => (
-              <ProductItem {...item} />
-            ))}
+    <>
+      <div className="mt-20 bg-yellow-400 py-3">
+        <h1 className="text-center uppercase text-white text-2xl font-bold">
+          All Products
+        </h1>
+      </div>
+      <div className="flex  gap-4  container mx-auto px-2 mt-5 relative  ">
+        {loading ? (
+          <Loader loading={loading} />
+        ) : (
+          <div className="flex-1">
+            <div className="grid lg:grid-cols-4  md:grid-cols-3 grid-cols-2">
+              {products.map((item) => (
+                <ProductItem {...item} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
