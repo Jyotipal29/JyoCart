8c8761df27e832defe8e5c296eb99bfd23ecc56c
jyoti pal <paljyoti0129@gmail.com>
2023-06-02 09:42:35 +0530

    created address model

---
+++ backend/model/address.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+
+  state: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+});
+
+const Address = mongoose.model("Address", addressSchema);
+
+module.exports = Address;
