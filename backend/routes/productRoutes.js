7554afb3568e0d3f4631fde8d94588fe42e44d64
jyoti pal <paljyoti0129@gmail.com>
2023-06-16 11:09:16 +0530

    created suggested products routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const {
   getAllProduct,
   getProduct,
+  getSuggestedProduct,
 } = require("../controller/productController");
 const router = express.Router();
 
 router.get("/", getAllProduct);
 router.get("/find/:id", getProduct);
+router.get("/:id", getSuggestedProduct);
 module.exports = router;
