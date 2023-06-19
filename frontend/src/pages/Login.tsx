7eb8156eeb126774f2335bad59566a3b050b5996
jyoti pal <paljyoti0129@gmail.com>
2023-06-19 21:43:42 +0530

    created guest login & updated style

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -43,29 +43,29 @@ const Login = () => {
   };
   return (
     <div className="container mx-auto px-8 mt-40 max-w-lg">
-      <div className="  bg-white shadow-xl rounded-sm  py-2 flex flex-col justify-start items-center">
-        <h1 className="uppercase text-2xl font-bold">Login</h1>
+      <div className="  bg-white shadow-xl rounded-md  py-2 flex flex-col justify-start items-center">
+        <h1 className="uppercase text-2xl font-bold font-lora">Login</h1>
         <form className=" w-3/4 my-2" onSubmit={submitHandler}>
           <div className=" py-2">
-            <label className="block uppercase font-semibold text-sm text-slate-400 mb-1 ">
+            <label className="block uppercase font-semibold text-sm text-slate-400 mb-1 font-lora ">
               email
             </label>
             <input
               value={email}
               type="email"
-              className="border-2 py-1 block w-full rounded-md outline-none"
+              className="border-2 py-1 block w-full  outline-none font-lora"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className=" py-2">
-            <label className="block uppercase text-sm text-slate-400 mb-1 font-semibold  ">
+            <label className="block uppercase text-sm text-slate-400 mb-1 font-semibold font-lora  ">
               password
             </label>
-            <div className="flex justify-center items-center border-2 rounded-md">
+            <div className="flex justify-center items-center border-2 ">
               <input
                 value={password}
                 type={open ? "text" : "password"}
-                className="py-1 block w-full rounded-md outline-none "
+                className="py-1 block w-full outline-none font-lora "
                 onChange={(e) => setPassword(e.target.value)}
               />
               <AiOutlineEye
@@ -74,10 +74,10 @@ const Login = () => {
               />
             </div>
           </div>
-          <div className="py-2 space-y-1">
+          <div className="py-2 space-y-3">
             <button
               type="submit"
-              className="bg-yellow-400 w-full py-1 uppercase text-white font-semibold text-xl rounded-md"
+              className="bg-yellow-400 font-lora w-full py-1 uppercase text-white font-semibold text-xl "
             >
               {loading ? (
                 <ClipLoader
@@ -92,22 +92,16 @@ const Login = () => {
               )}
             </button>
             <button
-              type="submit"
-              className="bg-yellow-200 w-full py-1 uppercase  font-semibold text-xl rounded-md"
+              type="button"
+              className=" border-2 border-yellow-400 font-lora w-full py-1 uppercase  font-semibold text-xl text-yellow-400 "
+              onClick={() => {
+                setEmail("guest@gmail.com");
+                setPassword("guest");
+              }}
             >
-              {loading ? (
-                <ClipLoader
-                  color="white"
-                  loading={loading}
-                  size={25}
-                  aria-label="Loading Spinner"
-                  data-testid="loader"
-                />
-              ) : (
-                "guest"
-              )}
+              guest
             </button>
-            <button onClick={() => navigate("/register")}>
+            <button onClick={() => navigate("/register")} className="font-lora">
               Don't have an account ? Register
             </button>
           </div>
