8fc652f3cd1e2dfa22bb155fc2bcc438871db881
jyoti pal <paljyoti0129@gmail.com>
2023-06-19 18:34:29 +0530

    updated hero section style

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,38 +3,41 @@ import modal from "../assets/ecomm-modal-1.webp";
 const Hero = () => {
   return (
     <div className="container mx-auto px-5 sm:px-2 mt-20">
-      <div className="flex justify-center items-center mt-2 bg-gray-200 rounded-2xl shadow-sm  px-16">
-        <div className=" w-1/2 flex flex-col items-start justify-start space-y-3 py-12">
-          <div className="  sm:leading-10 font-lora">
-            <span className="  text-xl  sm:text-3xl   md:text-6xl   uppercase font-semibold before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-white relative inline-block  ">
-              <span className="relative  ">let's</span>
+      <div className="relative flex flex-col sm:flex-row justify-center items-center mt-2 bg-gray-200 rounded-2xl shadow-sm px-16 -z-10">
+        <div className="w-full sm:w-1/1 flex flex-col items-start justify-start space-y-3 py-12 z-10 order-last sm:order-first">
+          <div className="sm:leading-10 font-lora">
+            <span className="text-xl sm:text-3xl md:text-6xl uppercase font-semibold before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-white relative inline-block">
+              <span className="relative">let's</span>
             </span>
-            <br></br>{" "}
-            <span className="  text-xl sm:text-3xl md:text-6xl  uppercase font-semibold ">
+            <br />
+            <span className="text-xl sm:text-3xl md:text-6xl uppercase font-semibold">
               explore
-            </span>{" "}
-            <br></br>{" "}
-            <span className=" text-xl sm:text-3xl md:text-6xl   uppercase font-semibold before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-400 relative inline-block ">
+            </span>
+            <br />
+            <span className="text-xl sm:text-3xl md:text-6xl uppercase font-semibold before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-400 relative inline-block">
               <span className="relative">unique</span>
-              {/* unique */}
-            </span>{" "}
-            <br></br>{" "}
-            <span className=" text-xl sm:text-3xl md:text-6xl   uppercase font-semibold ">
+            </span>
+            <br />
+            <span className="text-xl sm:text-3xl md:text-6xl uppercase font-semibold">
               clothes
             </span>
           </div>
-          <p className="  sm:text-xl font-semibold font-charm ">
+          <p className="sm:text-xl font-semibold font-charm">
             live for influential and innovative fashion
           </p>
-          <button className="bg-black text-white py-3 px-7 rounded-md">
-            <Link to="/products">shop now</Link>
-          </button>
+          <Link to="/products">
+            <button className="bg-black text-white py-3 px-7 rounded-md cursor-pointer">
+              shop now
+            </button>
+          </Link>
         </div>
-        <div className="  flex-1 flex justify-end items-center">
+
+        <div className=" absolute top-0 right-0 md:max-w-md w-full h-full">
+          <div className="absolute top-0 left-0 w-full h-full bg-gray-600 md:hidden opacity-50"></div>
           <img
             src={modal}
-            alt=" "
-            className="h-full w-full scale-150 md:scale-100"
+            alt=""
+            className="h-full w-full object-cover max-w-lg"
           />
         </div>
       </div>
