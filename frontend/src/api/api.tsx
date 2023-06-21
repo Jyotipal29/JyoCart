e3f31fbf8c4a2bd87784c3d55726955102c121b3
jyoti pal <paljyoti0129@gmail.com>
2023-06-21 18:46:31 +0530

    nodemon removed

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,1 +1,5 @@
 export const api = "http://localhost:4000/";
+
+// !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+//   ? "http://localhost:3002/api"
+//   : "https://ecommerce-api-u11b.onrender.com/api";
