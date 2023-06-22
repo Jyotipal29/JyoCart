d8b2c393f12a7342c2583d1ff112ead9df06d7f0
jyoti pal <paljyoti0129@gmail.com>
2023-06-22 15:18:00 +0530

    api updated

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,5 +1,2 @@
-export const api = "http://localhost:4000/";
-
-// !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-//   ? "http://localhost:3002/api"
-//   : "https://ecommerce-api-u11b.onrender.com/api";
+// export const api = "http://localhost:4000/";
+export const api = "https://jyo-cart-api.onrender.com/";
