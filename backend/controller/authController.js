3770f603faaecfc897fb19253a7494bcdda8de61
jyoti pal <paljyoti0129@gmail.com>
2023-06-22 14:37:38 +0530

    added try catch

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -44,20 +44,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  //check for user email
-  const user = await User.findOne({ email });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400).json({ message: "Unauthorized access" });
+    //check for user email
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "Unauthorized access" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
+  
 };
 
 const generateToken = (id) => {
